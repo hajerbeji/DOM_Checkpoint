b734815ae7b2c205c695bd3e77bdf414087b3e77
Hajer Beji <hajer.lasmar.beji.05@gmail.com>
2023-06-19 11:39:04 +0100

    first

--- script.js
+++ script.js
@@ -1,87 +1,45 @@
-// ------------------plus button-------------------
-let plus = document.querySelectorAll('.plus-btn');
-plus.forEach(function(plus){
-    plus.addEventListener('click',function(){
-        let valeur = plus.previousElementSibling;
-        let x = parseFloat(valeur.value);
-        let y = x + 1;
-        valeur.value = y;
+// variables
+let plus_btn = document.getElementsByClassName("plus-btn")
+let minus_btn = document.getElementsByClassName("minus-btn")
+let heart=document.getElementsByClassName("like")
+let quant=document.getElementsByClassName("Quant")
+let delete_btn=document.getElementsByClassName("delete")
+let item=document.getElementsByClassName("Item")
+let final_price=document.getElementById("finalPrice")
+let prices=[1379,176,500]
+
+// for Loop
+for(let index=0;index<plus_btn.length;index++){
+    // plus button
+
+    plus_btn[index].addEventListener("click",function(){
+        quant[index].value=Number(quant[index].value)+1
+        final_price.value=Number(final_price.value)+prices[index]
     })
-})
-// ------------------End plus button-------------------
 
+    // minus button
 
-
-// ------------------minus button-------------------
-let minus = document.querySelectorAll('.minus-btn');
-minus.forEach(function(minus){
-    minus.addEventListener('click',function(){
-        let valeur = minus.nextElementSibling;
-        let x = parseFloat(valeur.value);
-        let y = x - 1;
-        valeur.value = y;
-    })
-})
-// ------------------End minus button-------------------
-
-
-// ------------------Delete button-------------------
-let del=document.querySelectorAll('.delete');
-del.forEach(function(del) {
-    del.addEventListener('click', function() {
-      del.parentNode.remove();
-    })
-  });
-
-// ------------------End Delete button-------------------
-
-
-// ------------------clickable heart-------------------
-let heart=document.querySelectorAll('.like');
-heart.forEach(function(heart){
-    heart.addEventListener('click',function(){
-       heart.style.color = 'red';
-    })
-})
-// ------------------End clickable heart-------------------
-
-
-// ------------------Calculate Price-------------------
-
-// step 1 :create a function which calculate the total price    
-let prixTotal=document.getElementById('finalPrice');
-function final_price(){
-    let total=0;
-    let carts=document.querySelectorAll('.Item');
-    carts.forEach(function(cart){
-        let quantity=parseFloat(cart.querySelector('.Quant').value);
-        let prix=parseFloat(cart.querySelector('.price').textContent);
-        total=total+(quantity*prix);
-    })
-    prixTotal.value=total.toFixed(2);
-}
-
-// step2 : when we adjust the quantity through "+" and "-" buttons
-let plus_And_Minus=document.querySelectorAll('.quantity button');
-plus_And_Minus.forEach(function(button){
-    button.addEventListener('click',function(){
-        let input=button.parentNode.querySelector('.Quant');
-        let val=parseFloat(input.value);
-        if(button.classList.contains('minus-btn')){
-            val--;
-
-        }else if(button.classList.contains('plus-btn')){
-            val++;
+    minus_btn[index].addEventListener("click",function(){
+        if(quant[index].value!=="0"){
+            quant[index].value=Number(quant[index].value)-1
+            final_price.value=Number(final_price.value)-prices[index]
         }
-        final_price();
+        
     })
-})
 
-//step 3 : when we delete an item 
-let supp=document.querySelectorAll('.delete');
-supp.forEach(function(button){
-    button.addEventListener('click',function(){
-        final_price();
+    // heart button
+    heart[index].addEventListener("click",function(){
+        if(heart[index].style.color==="red"){
+            heart[index].style.color="black"
+        }
+        else{
+            heart[index].style.color="red"
+        }
     })
-})
-// ------------------End Calculate Price-------------------
+
+    // delete button
+    delete_btn[index].addEventListener("click",function(){
+        item[index].style="display:none;"
+        final_price.value=Number(final_price.value)-prices[index]*Number(quant[index].value)
+    })
+}
